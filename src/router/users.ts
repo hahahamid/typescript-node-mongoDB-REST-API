f60469964f75b49cd90244415470ca93ec480a5a
Miran Hamid <miranhamid2002@gmail.com>
2023-08-26 05:19:56 +0530

    Initial Commit

---
+++ src/router/users.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
+import { isAuthenticated, isAdmin } from "../middlewares";
+
+export default (router: express.Router) => {
+  router.get("/users", isAuthenticated, getAllUsers);
+  router.delete("/users/:id", isAuthenticated, isAdmin, deleteUser);
+  router.patch("/users/:id", isAuthenticated, isAdmin, updateUser);
+};
